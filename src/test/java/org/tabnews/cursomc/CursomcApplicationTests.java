c1cf234c9bc2dacccf929269a468d010830275c2
Regis Ribeiro <reginaldobr.araujo@gmail.com>
2023-02-20 10:08:18 -0300

    Versao Inicial do Projeto Criado

---
+++ src/test/java/org/tabnews/cursomc/CursomcApplicationTests.java
@@ -0,0 +1,13 @@
+package org.tabnews.cursomc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursomcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
